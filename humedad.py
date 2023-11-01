4b60b505d34d562a4665de686f1fb182854ffc2a
Nunjabes <118215626+Nunjabes@users.noreply.github.com>
2023-11-01 17:47:16 +0100

    Actualizar humedad.py

---
+++ humedad.py
@@ -0,0 +1,7 @@
+humidity = [nivel1, nivel2, nivel3, ...]  # Asegúrate de reemplazar nivel1, nivel2, nivel3, ... con los datos reales de humedad.
+
+nivel_mas_bajo = min(humidity)
+nivel_mas_alto = max(humidity)
+
+print("El nivel de humedad más bajo es:", nivel_mas_bajo)
+print("El nivel de humedad más alto es:", nivel_mas_alto)
